7e1de9b2bb1f73dd4635cc14fcddc62a045fee64
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-21 15:56:08 +0600

    Sign Up Page Validation Complete

--- src/components/SignUpPage/SignUpPage.jsx
+++ src/components/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,46 @@
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../../Firebase/firebase.config";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const auth = getAuth(app);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const accepted = e.target.terms.checked;
+    console.log(email, password, accepted, name);
+    setError("");
+    setSuccess("");
+    {
+      if (password.length < 6) {
+        setError("Password should be at least 6 characters");
+        return;
+      } else if (!accepted) {
+        setError("Please Accept Our Conditions");
+        return;
+      } else if (!/[A-Z]/.test(password)) {
+        setError("! Your Password Must be one uppercase");
+        return;
+      }
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess("your account successfully created");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
   return (
     <div className="form__container">
-      <form className="container formBG mx-auto">
+      <form onSubmit={handleSignUp} className="container formBG mx-auto">
         <h1 className="text-center text-3xl font-bold mb-3">Sign Up </h1>
         <div className="mb-3">
           <p>Your Name</p>
@@ -35,13 +72,19 @@ const SignUpPage = () => {
             required
           />
         </div>
-        <div className="flex items-center justify-between">
+        <div>
+          {success && <p className="text-green-700">{success}</p>}
+          {error && <p className="text-red-700">{error}</p>}
+        </div>
+        <div className="flex items-center justify-between mt-3">
           <div className="flex items-center gap-2">
             <input type="checkbox" id="terms" className="checkbox" />
-            <label htmlFor="terms">Accept our terms and conditions</label>
+            <label className="cursor-pointer" htmlFor="terms">Accept our terms and conditions</label>
           </div>
           <div>
-            <Link to="/login" className="underline">Already Have an Account?</Link>
+            <Link to="/login" className="hover:no-underline underline">
+              Already Have an Account?
+            </Link>
           </div>
         </div>
         <button className="btn btn-success w-full mt-4">Sign Up</button>
